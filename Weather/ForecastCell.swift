b5fcb268166f5051dd82b27365fbf9ada7a4c6fa
ioshero <handsomeman1011@gmail.com>
2017-02-22 14:49:44 +0100

    backup

---
+++ Weather/ForecastCell.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastCell.swift
+//  Weather
+//
+//  Created by ioshero on 5/14/15.
+//  Copyright (c) 2015 ioshero. All rights reserved.
+//
+
+import UIKit
+
+class ForecastCell: UITableViewCell {
+
+    @IBOutlet weak var iWeather: UIImageView!
+    @IBOutlet weak var lDay: UILabel!
+    @IBOutlet weak var lWeatherDesc: UILabel!
+    @IBOutlet weak var lTemperature: UILabel!
+}
